9faf496664ff2ed242b982f0eb3a380c5fb4068f
Timothy Tan <timtan93@gmail.com>
2018-12-02 09:01:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|call| call.length > 4}# code an argument here
-  # Your code here
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(cheese)# code an argument here
