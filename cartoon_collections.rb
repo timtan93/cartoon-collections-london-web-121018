8963e54396fdd7af2ae948725d4c47317330c979
Timothy Tan <timtan93@gmail.com>
2018-12-02 08:59:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map!{|x| x.capitalize + "!"}
+  planeteer_calls.map{|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
